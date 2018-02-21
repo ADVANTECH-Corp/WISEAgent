1120c7e6fd9d5612b688e61fa262d0a0185f5440
Scott Chang <scott68.chang@advantech.com.tw>
2018-02-21 14:32:14 +0800

    update svnversion.h to V 3.3.20

--- Include/svnversion.h
+++ Include/svnversion.h
@@ -2,6 +2,6 @@
 #define __SVN_REVISION_H__
 #define MAIN_VERSION 3
 #define SUB_VERSION 3
-#define BUILD_VERSION 11
-#define SVN_REVISION 7683
+#define BUILD_VERSION 20
+#define SVN_REVISION 8071
 #endif /* __SVN_REVISION_H__ */
